fe46e8084fe4dd91772bce368524293b12394810
omar-sayed97 <81324216+omar-sayed97@users.noreply.github.com>
2021-06-21 15:39:31 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ * spi slaveeeeee.c
+ *
+ * Created: 6/16/2021 12:56:08 PM
+ * Author : omar.sayed
+ */ 
+
+#include "SPI.h"
+#define F_CPU  16000000
+#include <util/delay.h>
+
+
+int main(void)
+{
+	
+	
+	DIO_SetPortDir(DIO_PORTD,DIO_PORT_OUTPUT);
+	SPI_Master_Init();
+	
+	SPI_Slave_Init();
+	
+	
+	while (1)
+	{
+		SPI_SLAVE_OUTPUT();
+	}
+}
---
+++ salve_output.h
@@ -0,0 +1,18 @@
+/*
+ * salve_output.h
+ *
+ * Created: 6/17/2021 12:02:27 AM
+ *  Author: omar.sayed
+ */ 
+
+
+#ifndef SALVE_OUTPUT_H_
+#define SALVE_OUTPUT_H_
+
+#include "DIO.h"
+
+void SPI_SLAVE_OUTPUT(void);
+
+
+
+#endif /* SALVE_OUTPUT_H_ */
